cb70ba0ac2e60d9474a152a9eba96ed740c2aab7
Piotrek Dąbrowski <floubadour@gmail.com>
2015-12-14 16:36:38 +0100

    improvements for TT print and other stuff

--- mainCtrl.js
+++ mainCtrl.js
@@ -16,12 +16,26 @@ angular.module('tt-bc')
 	var days = calendarHelper.getMonthView($scope.timesheet.calendarDay);
 	$scope.days = calendarService.fillDays(days);
 
+	function sumHours() {
+		var workingDays = $scope.days.count(function(el) {
+			return el.dayType == null || el.dayType < 1;
+		});
+
+		return workingDays * 8;
+	}
+
 	$scope.saveBasicData = function() {
 		localStorage.setItem('basicData', JSON.stringify($scope.timesheet.basic));
 	};
 
 	$scope.saveAndPrint = function() {
 		$scope.timesheet.days = $scope.days;
+		var date = moment();
+		$scope.timesheet.date = {
+			month: date.month() + 1,
+			year: date.year()
+		};
+		$scope.timesheet.hours = sumHours();
 		localStorage.setItem('timesheet', JSON.stringify($scope.timesheet));
 		window.location = './print.html';
 	};
@@ -38,4 +52,26 @@ angular.module('tt-bc')
 	}
 
 	$scope.timesheet = loadBasicData();
-}]);
+}])
+.filter('monthName', function() {
+  var monthsHashTable = {
+    '1': 'styczeń',
+    '2': 'luty',
+    '3': 'marzec',
+    '4': 'kwiecień',
+    '5': 'maj',
+    '6': 'czerwiec',
+    '7': 'lipiec',
+    '8': 'sierpień',
+    '9': 'wrzesień',
+    '10': 'październik',
+    '11': 'listopad',
+    '12': 'grudzień'
+  };
+  return function(monthNo) {
+    if(!monthNo)
+      return null;
+
+    return monthsHashTable[monthNo];
+  };
+});
